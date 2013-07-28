e1854f669d74032efc320eb99c609b292d693ed0
Hans <hanstrax@gmail.com>
2013-07-28 19:35:16 -0400

    first commit: basic-layout, bootstrap3, haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Topvision::Application.config.secret_key_base = 'bbb0e4dd3932ac510b711a5639d8caf667322bb12d7652e86a284244848c1d8ef766b1b4ad1367fbae970398e4f097ab5e97736ea0fa3ffa0141ad764104c3ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Topvision::Application.config.session_store :cookie_store, key: '_topvision_session'
